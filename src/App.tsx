348dc25e08e47c9dd934052fb6577216cea9b2d6
Hant <hangem422@gmail.com>
2021-06-07 01:36:41 +0900

    Function - create body section

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useMemo } from 'react';
 
 import Artwork from 'modules/store/hook/artwork';
+import cx from 'styles';
 
 function App() {
   const artwork = Artwork();
+
+  const appClassName = useMemo(() => cx('App'), []);
+  const bodyClassName = useMemo(() => cx('body'), []);
 
   useEffect(() => {
     artwork.initialize();
   }, []);
 
-  return <div className="App" />;
+  return (
+    <div className={appClassName}>
+      <div className={bodyClassName} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/styles/index.ts
@@ -0,0 +1,6 @@
+import classNames from 'classnames/bind';
+import styles from 'styles/main.module.scss';
+
+const cx = classNames.bind(styles);
+
+export default cx;
